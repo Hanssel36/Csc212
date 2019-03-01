dd4d4a8172484669c2d3112cc2b842026fefd1b7
Hanssel Hinojosa <hanssel36@gmail.com>
2019-02-28 22:49:29 -0500

    Backup

--- Project3/sequence2.cpp
+++ Project3/sequence2.cpp
@@ -1,9 +1,10 @@
 #include "sequence2.h"
-using main_savitch_4;
-namespace main_savitch_4
-{
-  sequence::sequence(size_type initial_capacity = DEFAULT_CAPACITY){
-    data = new value_type[initial_capacity];
+#include <algorithm>
+using namespace std;
+
+namespace main_savitch_4{
+  sequence::sequence(size_type initial_capacity){
+    data =  new value_type[initial_capacity];
     capacity = initial_capacity;
     used = 0;
     }
@@ -11,27 +12,12 @@ namespace main_savitch_4
     data = new value_type[source.capacity];
     capacity = source.capacity;
     used = source.used;
+    copy(source.data,source.data+used,data);
     }
-  sequence::~sequence(){
-    delete [] data;
-    }
-     void sequence::operator=(const sequence& source){
-       value_type*new_data;
-       if(this == &source){
-         return;
-         }
-         if(capacity != source.capacity){
-           new_data = new value_type[source.capacity];
-           delete [] data;
-           data = new_data;
-           capacity = source.capacity;
-           }
-            used = source.used;
-           copy(source.data,source.data + used,data);
-       }
 
-       void attach(const value_type& entry){
-         if(is_item() == true){
+
+  void sequence::attach(const value_type& entry){
+    if(is_item() == true){
       used++;
     for(int i = used; i > current_index; --i){
 
@@ -49,9 +35,11 @@ namespace main_savitch_4
       used++;
       current_index = 0;
       }
-         }
-       void insert(const value_type& entry){
-         if(is_item() == true){
+  }
+
+
+  void sequence::insert(const value_type& entry){
+    if(is_item() == true){
     for(int i = used; i > current_index; --i){
       data[i] = data[i-1];
       }
@@ -70,22 +58,26 @@ namespace main_savitch_4
       used++;
       current_index = 0;
         }
-         }
-    sequence::value_type sequence::current( ) const{
+    }
+
+
+
+sequence::size_type sequence::size( ) const{
+    return used;
+    }
+
+bool sequence::is_item( ) const{
+  if(size() > 0 && current_index < used){
+    return true;
+    }else{
+      return false;
+      }
+  }
+sequence::value_type sequence::current( ) const{
   if(is_item() == true){
     return data[current_index];
   }else{
     return false;
     }
-  }
-      sequence::size_type sequence::size( ) const{
-    return used;
-    }
-bool sequence::is_item( ) const{
-  if(size() > 0 && current_index < used){
-    return true;
-    }else{
-      false;
-      }
   }
   }
