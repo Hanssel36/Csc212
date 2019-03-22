a131dc95ca49d4a99d8e12fc282bc8e1a9711e00
Hanssel36 <Hanssel36@gmail.com>
2019-03-21 22:12:00 -0400

    Backup

---
+++ Project5/rec_fun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void binary_print(ostream& outs, unsigned int n){
+	if(n == 0){
+		outs << 0;
+	}else{
+		binary_print(cout,n/2);
+		outs << n%2;
+	}
+
+}
+
+int main(){
+
+	binary_print(cout,0);
+
+}
